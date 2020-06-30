42d38f65ceb3eaab7b0ee96c2dc4960fc7d9773f
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-06-30 20:11:46 +0900

    set up messages tableView

--- FireChat/FireChat/Controller/ConversationsController.swift
+++ FireChat/FireChat/Controller/ConversationsController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
+private let reuseIdentifier = "ConversationCell"
+
 class ConversationsController: UIViewController {
     
     // MARK: - Properties
+    
+    private let tableView = UITableView()
     
     // MARK: - Lifecycle
     
@@ -23,7 +27,7 @@ class ConversationsController: UIViewController {
     // MARK: - Selectors
     
     @objc func showProfile(){
-        
+        print(123)
     }
     
     // MARK: - Helpers
@@ -32,9 +36,22 @@ class ConversationsController: UIViewController {
         view.backgroundColor = .white
         
         configureNavigationBar()
+        configureTableView()
         
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
+    
+    func configureTableView(){
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
     }
     
     func configureNavigationBar(){
@@ -44,6 +61,7 @@ class ConversationsController: UIViewController {
         appearance.backgroundColor = .systemPurple
         
         navigationController?.navigationBar.standardAppearance = appearance
+        // スクロールした際に、sizeがコンパクトになる
         navigationController?.navigationBar.compactAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         
@@ -54,4 +72,27 @@ class ConversationsController: UIViewController {
         
         navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
     }
+}
+
+// MARK: - UITabelViewDataSource
+
+extension ConversationsController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Test Cell"
+        return cell
+    }
+}
+
+// MARK: - UITableViewDalegate
+
+extension ConversationsController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("DEBUG: indexPath is \(indexPath.row)")
+    }
+    
 }
