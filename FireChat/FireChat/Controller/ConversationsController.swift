b6c53da4e7d9fe3dbec6bd661c02dc6690535926
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-06-30 19:36:59 +0900

    configure project and set up home screen

--- FireChat/FireChat/Controller/ConversationsController.swift
+++ FireChat/FireChat/Controller/ConversationsController.swift
@@ -16,7 +16,25 @@ class ConversationsController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func showProfile(){
+        
     }
     
     // MARK: - Helpers
+    
+    func configureUI(){
+        view.backgroundColor = .white
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
 }
