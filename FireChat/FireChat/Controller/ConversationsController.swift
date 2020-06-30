a1585d379a21deb2f4da5273120b949dd9eacf63
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-06-30 19:07:27 +0900

    make some groups

---
+++ FireChat/FireChat/Controller/ConversationsController.swift
@@ -0,0 +1,22 @@
+//
+//  ConversationsController.swift
+//  FireChat
+//
+//  Created by 宮本一成 on 2020/06/30.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Helpers
+}
