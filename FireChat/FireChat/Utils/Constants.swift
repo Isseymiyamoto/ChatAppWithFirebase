5cf72602af5fab5f247cb8e404ad5cd1d13ae790
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-08-02 11:55:29 +0900

    message model and upload message to firestore

---
+++ FireChat/FireChat/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  FireChat
+//
+//  Created by 宮本一成 on 2020/08/02.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let COLLECTION_MESSAGES = Firestore.firestore().collection("messages")
